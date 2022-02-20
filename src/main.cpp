caac76ee2eed638dc06c61f97b878beac16ebd55
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-02-20 13:20:48 +0330

    Write three test case in three function in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,97 @@
 #include <string>   // For cout string
 #include "stack.h"  // For stack class
 
+void my()
+{
+    std::cout << "My test case : \n";
+    stack s;
+    if (s.isEmpty())
+        std::cout << "Yes, your stack is empty\n";
+    else
+        std::cout << "No, your stack is not empty\n";
+    try
+    {
+        s.pop();
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    s.push(1);
+    s.push(3);
+    s.push(5);
+    s.push(7);
+    try
+    {
+        std::cout << "popped : " << s.pop() << "\n";
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    s.push(9);
+    try
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            int temp = s.pop();
+            std::cout << "popped : " << temp << "\n";
+        }
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+void you()
+{
+    std::cout << "Your test case : \n";
+    stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    try
+    {
+        std::cout << s.pop() << " popped from stack\n";
+        std::cout << "Top element is : " << s.peek() << "\n";
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+void dr()
+{
+    std::cout << "Dr. test case : \n";
+    stack s;
+    std::string str = "(((a+b)*c+d-e)/(f+g)-(h+j)*(k-l))/(m-n)";
+    bool Bool = false;
+    int temp;
+    try
+    {
+        for (int i = 0; i < str.size(); i++)
+        {
+            if (str[i] == '(')
+                s.push(i);
+            else if (str[i] == ')')
+            {
+                temp = s.pop();
+                std::cout << "(" << temp << "-" << i << ")\n";
+            }
+        }
+    }
+    catch (const std::invalid_argument &e)
+    {
+        Bool = true;
+    }
+    if (!s.isEmpty() || Bool)
+        std::cout << "The " << str << " is wrong :(\n";
+    else
+        std::cout << "The \"" << str << "\" is correct :)\n";
+}
+
 int main()
 {
     return 0;
