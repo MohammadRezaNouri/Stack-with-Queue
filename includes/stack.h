f4a0a6de88e9658ff70cdd05fc78ae3939e30785
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-02-20 13:11:54 +0330

    Definition of class stack in stack.h

--- includes/stack.h
+++ includes/stack.h
@@ -1,4 +1,17 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <queue>
+
+class stack
+{
+private:
+    std::queue<int> q1, q2;
+public:
+    bool isEmpty();
+    void push(int);
+    int pop();
+    int peek();
+};
+
 #endif
