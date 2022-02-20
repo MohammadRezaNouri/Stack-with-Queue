7c4f4e7093b0e3791eb524e9922aa713ee2e0dfb
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-02-20 13:15:59 +0330

    Implementing the push and pop functions in stack.cpp

--- src/stack.cpp
+++ src/stack.cpp
@@ -10,3 +10,34 @@ bool stack::isEmpty()
     }
     return false;
 }
+
+void stack::push(int d)
+{
+    q1.push(d);
+}
+
+int stack::pop()
+{
+    if (q1.empty())
+    {
+        throw std::invalid_argument("Stack is empty!");
+    }
+    int temp = q1.back();
+    if (q1.size() == 1)
+    {
+        q1.pop();
+        return temp;
+    }
+    while (q1.size() != 1)
+    {
+        q2.push(q1.front());
+        q1.pop();
+    }
+    q1.pop();
+    while (q2.size() != 0)
+    {
+        q1.push(q2.front());
+        q2.pop();
+    }
+    return temp;
+}
